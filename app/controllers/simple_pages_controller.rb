3d18f4c0c509eacda7ed1350c56b06aa7b43b9ca
brunoagcorreia <brunocorreia@gmail.comgit>
2018-06-05 16:54:30 +0200

    4.5 landing pg updated;  created product, simple_pages partials

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,13 +1,9 @@
 class SimplePagesController < ApplicationController
   def index
+    @products = Product.all
   end
-end
-
-class SimplePagesController < ApplicationController
 
   def landing_page
-    @featured_product_1 = Product.first
-    @featured_product_2 = Product.second
-    @featured_product_3 = Product.last
+    @products = Product.limit(4)
   end
 end
