51bb4955e98e908789ec897edde59c322115e580
brunoagcorreia <brunocorreia@gmail.comgit>
2018-07-12 01:26:15 +0200

    5.2 added search form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  def self.search(search_term)
+    Product.where("name LIKE ?", "%#{search_term}%")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
 
-  #root 'simple_pages#landing_page'
-  root 'products#index'
+  root 'simple_pages#landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
