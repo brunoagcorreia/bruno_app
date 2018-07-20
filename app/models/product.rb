332d44a6d1b672f5f8e5cb79b85de8bf3d5f9f60
brunoagcorreia <brunocorreia@gmail.comgit>
2018-07-20 07:55:01 +0200

    5.2 product model fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
   has_many :orders
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    if Rails.env.development?
+      # Dev Mode (sqlite3)
+      Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      # Production or test mode (PostgreSQL)
+      Product.where("name ilike ?", "%#{search_term}%")
+    end
   end
 end
