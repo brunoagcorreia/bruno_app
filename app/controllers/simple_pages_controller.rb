16d594d496cd7a8f982e7a5aaea7bbf1c1fd6b77
brunoagcorreia <brunocorreia@gmail.comgit>
2018-07-20 22:03:49 +0200

    5.3 sendgrid plugin added

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -6,4 +6,14 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.all
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'bruno@correia.space',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
 end
