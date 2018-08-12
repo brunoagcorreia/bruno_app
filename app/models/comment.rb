acd4203055151484813fb106292b032215ea8a63
brunoagcorreia <brunocorreia@gmail.comgit>
2018-08-12 13:08:02 +0200

    5.7 comment and product model edited

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  # Called by e.g. <%= @product.comments.rating_desc.first %>
+  scope :rating_desc, -> { order(rating: :desc) }
+  # Called by e.g. <%= @product.comments.rating_asc.first %>
+  scope :rating_asc, -> { order(rating: :asc) }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   has_many :orders
   has_many :comments
+
   def self.search(search_term)
     if Rails.env.development?
       # Dev Mode (sqlite3)
@@ -10,4 +11,20 @@ class Product < ApplicationRecord
       Product.where("name ilike ?", "%#{search_term}%")
     end
   end
+
+  # Called by <%= @product.highest_rating_comment %>
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  # Called by <%= @product.lowest_rating_comment %>
+  def lowest_rating_comment
+    comments.rating_desc.last
+  end
+
+  # Called by <%= @product.average_rating %>
+  def average_rating
+    comments.average(:rating).to_f
+  end
+
 end
