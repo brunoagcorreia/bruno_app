67ab486e4e701a41e25549fcde56200ab10f1334
brunoagcorreia <brunocorreia@gmail.comgit>
2018-08-14 23:43:24 +0200

    5.8 comments controller and views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+
+  resources :products do
+    resources :comments
+  end
+
   resources :users
-  resources :products
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
   get 'simple_pages/gdpr'
