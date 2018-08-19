e641a26854dcb588f35b20fc02469f30192bc753
brunoagcorreia <brunocorreia@gmail.comgit>
2018-08-19 18:48:23 +0200

    5.11 registration mailer added, layout edited

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
          to: 'bruno@correia.space',
          subject: "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "Magical Music Store"
+    mail(to: user.email,
+         subject: "Thanks for your registration at #{@appname}!")
+  end
 end
