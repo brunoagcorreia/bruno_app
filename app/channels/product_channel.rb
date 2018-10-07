2e46c6b1d00a81d53e336df8e80e66af629da7d9
brunoagcorreia <brunocorreia@gmail.comgit>
2018-10-07 11:45:51 +0200

    6.13 cable.yml edited

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -2,10 +2,16 @@
 # Action Cable runs in a loop that does not support auto reloading.
 class ProductChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "product_channel"
+    # stream_from "product_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  # 'data' argument will contain the product ID.
+  def listen(data)
+    stop_all_streams
+    stream_for data["product_id"]
   end
 end
